13179f8fe9d9c404657150bfc2de5c3a656c64c7
Herezzy <cristian.ars@hotmail.com>
2024-04-30 16:56:27 -0500

    ultimo comitt

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Document</title>hola mundo
-
-=======
     <title>Document</title>
     <form action="">
         <label for="">nombres</label>
@@ -14,10 +10,12 @@
         <label for="">apellidos</label>
         <input type="text">
     </form>
->>>>>>> 7c3565823c23e713b813913a46acc1ae6adb8118
 </head>
 <body>
-    asdasdsdf
-    asd
+<?php
+
+echo"hola mundo "
+?>
+
 </body>
 </html>
