b033ab318752ecdd7302df3ea8983ec2d3c4d3b4
andres <felipecano09@gmail.com>
2024-05-02 15:14:22 -0500

    menu

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php include("menu\menu.php")?>
     <form action="">
         <label for="">nombres</label>
         <input type="text">
