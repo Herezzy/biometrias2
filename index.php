aa893dc118bc76c6c171661366591ec797178115
andres <felipecano09@gmail.com>
2024-05-03 14:01:53 -0500

    se crea login

--- index.php
+++ index.php
@@ -3,27 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php include("menu\menu.php")?>
-    <form action="">
-        <label for="">nombres</label>
-        <input type="text">
-        <label for="">apellidos</label>
-        <input type="text">
-    </form>
+    <title>Login</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-
-
-    
-
-<?php
-$saludo="hola";
-$suma=2+2;                                                                                      
-echo $saludo;
-echo $suma;
-
-?>
-    
+    <div class="login-container">
+        <form class="login-form" method="POST" action="evaluar.php">
+            <div class="logo-container">
+                <img src="migracion_logo.png" alt="Logo">
+            </div>
+            <h2>Biometrias</h2>
+            <div class="input-group">
+                <label for="documento">Número de Documento:</label>
+                <input type="text" id="documento" name="documento" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+    </div>
 </body>
 </html>
